0aa2452ad9f4888171e38902d34d149df0b25096
louise <69238115+conraduouou@users.noreply.github.com>
2026-07-30 01:08:20 +0800

    add aliases for fast file opening and editing with fd, fzf, and vim for windows

--- profile.ps1
+++ profile.ps1
@@ -8,6 +8,26 @@ function vifm {
     & "$HOME\bin\vf.ps1" @args
 }
 
+function ff {
+    $files = @(fd)
+
+    $file = $files | fzf
+
+    if ($file) {
+        vim $file
+    }
+}
+
+function of {
+    $files = @(fd)
+
+    $file = $files | fzf
+
+    if ($file) {
+        . $file
+    }
+}
+
 Remove-Alias -Name cd
 Set-Alias cd z
 
@@ -21,3 +41,6 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # For PSReadLine controls
 Set-PSReadLineKeyHandler -Chord Alt+j -Function NextHistory
 Set-PSReadLineKeyHandler -Chord Alt+k -Function PreviousHistory
+
+# For fzf controls and options
+# $env:FZF_DEFAULT_OPTS = '--bind=alt-j:down --bind=alt-k:up --preview="cmd /c bat --color=always --style=numbers {}" --preview-window=right:60%'
