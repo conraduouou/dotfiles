ba0f5a3a39e884229370ee48e159b2e5558ca87c
Lalu <louise.rafael.lalu@accenture.com>
2026-08-06 11:38:07 +0800

    disable background highlight groups for nvim to be transparent

--- config/nvim/lua/config/colorscheme.lua
+++ config/nvim/lua/config/colorscheme.lua
@@ -7,3 +7,9 @@ if vim.fn.has("win32") == 0 then
     vim.cmd.colorscheme("oxocarbon")
 end
 
+vim.cmd([[
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none
+    highlight NonText ctermbg=none
+]])
