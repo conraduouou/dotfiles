cb51d1f096e90006d06a190a857e3027a4736721
Lalu <louise.rafael.lalu@accenture.com>
2026-08-07 01:58:23 +0800

    add quick launch mappings in glazewm; add msedge in windows path

--- config/powershell/path.ps1
+++ config/powershell/path.ps1
@@ -2,3 +2,4 @@
 
 $Env:Path = "$HOME\.local\bin;" + $Env:Path
 $Env:Path = "$HOME\Tools\vifm;" + $Env:Path # temporary until Chocolatey has vifm v0.14
+$Env:Path = "C:\Program Files (x86)\Microsoft\Edge\Application;" + $Env:Path # for msedge
