5075963aa7746a406903b3f688dc042846b7fddd
Lalu <louise.rafael.lalu@accenture.com>
2026-08-20 18:38:04 +0800

    fix nvim pane open on wezterm windows focusing on pane

--- bin/nvim-open-wezterm.cmd
+++ bin/nvim-open-wezterm.cmd
@@ -1,8 +1,25 @@
 @echo off
 setlocal
 
+rem Set current tab id by finding it through the WEZTERM_PANE env variable
+set "tab_id="
+for /F "tokens=2,3 skip=1" %%i in ('wezterm cli list') do (
+    if "%WEZTERM_PANE%"=="%%j" (
+        set "tab_id=%%i"
+        goto after_loop
+    )
+)
+
+:after_loop
+
+rem If tab_id was not assigned for some reason, we don't like that, exit away
+if not defined tab_id (
+    echo "WezTerm tab ID was not successfully identified. Please inspect."
+    exit /b 0
+)
+
 rem One Neovim named pipe per WezTerm pane.
-set "pipe=\\.\pipe\nvim-wezterm-%WEZTERM_PANE%"
+set "pipe=\\.\pipe\nvim-wezterm-%tab_id%"
 
 rem No arguments: start a server in the current pane.
 if "%~1"=="" goto start_nvim
@@ -25,4 +42,3 @@ goto :eof
 
 :start_nvim
 nvim.exe --listen "%pipe%"
-goto :eof
