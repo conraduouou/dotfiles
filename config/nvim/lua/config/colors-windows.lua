a46a8b3ed7f4ff961d76256d68e85b063e53ea57
Louise <louiserafaellalu@gmail.com>
2026-08-22 18:16:18 +0800

    revert macos colorscheme and refactor color files

---
+++ config/nvim/lua/config/colors-windows.lua
@@ -0,0 +1,14 @@
+-- ======================
+-- Windows color scheme
+-- ======================
+
+vim.cmd([[
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none
+    highlight NonText ctermbg=none
+]])
+
+vim.api.nvim_set_hl(0, "FzfLuaNormal", {
+    bg = "#202020",
+})
--- config/nvim/lua/config/colorscheme.lua
+++ config/nvim/lua/config/colorscheme.lua
@@ -4,16 +4,8 @@
 
 -- Determine OS
 if vim.fn.has("win32") == 0 then
-    vim.cmd.colorscheme("oxocarbon")
+    require("config.colors-macos")
+else
+    require("config.colors-windows")
 end
 
-vim.cmd([[
-    highlight Normal guibg=none
-    highlight NonText guibg=none
-    highlight Normal ctermbg=none
-    highlight NonText ctermbg=none
-]])
-
-vim.api.nvim_set_hl(0, "FzfLuaNormal", {
-    bg = "#202020",
-})
