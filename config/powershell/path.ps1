2473ea13597fc40ec908fbf4009e35537e324b30
Lalu <louise.rafael.lalu@accenture.com>
2026-09-09 14:38:12 +0800

    add visual studio in glazewm binding

--- config/powershell/path.ps1
+++ config/powershell/path.ps1
@@ -1,6 +1,15 @@
 # path.ps1
 
 $Env:Path = "$HOME\.local\bin;" + $Env:Path
-$Env:Path = "$HOME\Tools\vifm;" + $Env:Path # temporary until Chocolatey has vifm v0.14
-$Env:Path = "$HOME\Tools\gum;" + $Env:Path # temporary until Chocolatey has gum v2.0.0
-$Env:Path = "C:\Program Files (x86)\Microsoft\Edge\Application;" + $Env:Path # for msedge
+
+# TODO: temporary until Chocolatey has vifm v0.14
+$Env:Path = "$HOME\Tools\vifm;" + $Env:Path
+
+# TODO: temporary until Chocolatey has gum v2.0.0
+$Env:Path = "$HOME\Tools\gum;" + $Env:Path
+
+# for msedge
+$Env:Path = "C:\Program Files (x86)\Microsoft\Edge\Application;" + $Env:Path
+
+# for devenv, Visual Studio
+$Env:Path = "C:\Program Files\Microsoft Visual Studio\18\Enterprise\Common7\IDE"
